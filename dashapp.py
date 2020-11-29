9dafd56d83e60dbaeda972b3174345c2bb36dc28
RobinKongNingLo <46904895+RobinKongNingLo@users.noreply.github.com>
2020-11-28 21:25:52 -0500

    Add files via upload

---
+++ dashapp.py
@@ -0,0 +1,21 @@
+import numpy as np
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.express as px
+import plotly.graph_objects as go
+import pandas as pd
+import json
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+server = app.server
+
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
